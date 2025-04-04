a3cfd601e1d30cbbc011aa1c2b3d681671405809
mle2718 <Min-Yang.Lee@noaa.gov>
2025-04-04 14:48:41 -0500

    typo fix

--- writing/batch_RF_run.sh
+++ writing/batch_RF_run.sh
@@ -1,3 +1,5 @@
-RScript ./estimate_randomforest.R --vanilla > estimate_randomforest.log
-RScript ./estimate_randomforest_nocluster.R --vanilla > estimate_randomforest_nocluster.log
-RScript ./estimate_5class_randomforest_nocluster.R --vanilla > estimate_5class_randomforest_nocluster.log
+Rscript ./estimate_randomforest.R --vanilla > estimate_randomforest.log
+
+Rscript ./estimate_randomforest_nocluster.R --vanilla > estimate_randomforest_nocluster.log
+
+Rscript ./estimate_5class_randomforest_nocluster.R --vanilla > estimate_5class_randomforest_nocluster.log
