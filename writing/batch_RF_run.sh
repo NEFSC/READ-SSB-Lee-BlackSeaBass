05791587811c8c5bc8ce184464c6a967fc7984e0
mle2718 <Min-Yang.Lee@noaa.gov>
2025-04-04 13:53:46 -0500

    adjust knit script. Write batch_RF_run.sh script

---
+++ writing/batch_RF_run.sh
@@ -0,0 +1,3 @@
+RScript ./estimate_randomforest.R --vanilla > estimate_randomforest.log
+RScript ./estimate_randomforest_nocluster.R --vanilla > estimate_randomforest_nocluster.log
+RScript ./estimate_5class_randomforest_nocluster.R --vanilla > estimate_5class_randomforest_nocluster.log
--- writing/knit_script.R
+++ writing/knit_script.R
@@ -1,11 +1,16 @@
 # Run this from terminal with:
 # > Rscript knit_script.R
+# But it's better to run the batch_RF_run.sh file, because that logs individual scripts
+
 library("here")
 library("rmarkdown")
 
 here::i_am("writing/knit_script.R")
 
+source(here("writing","estimate_randomforest_nocluster.R")
 
+       
+source(here("writing","estimate_randomforest.R")
 
 ##################################################################################
 rmarkdown::render(here::here("writing","estimate_randomforest.Rmd"),
@@ -15,6 +20,6 @@ rmarkdown::render(here::here("writing","estimate_randomforest.Rmd"),
 
 
 ##################################################################################
-rmarkdown::render(here::here("writing","reading_ranger_results.Rmd"),
-                  output_file = here::here("writing","reading_ranger_results.html"))
+# rmarkdown::render(here::here("writing","reading_ranger_results.Rmd"),
+#                   output_file = here::here("writing","reading_ranger_results.html"))
 ##################################################################################
