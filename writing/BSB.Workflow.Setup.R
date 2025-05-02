c2779adc261de93356ab9b9233ba1e6aa4a09e05
mle2718 <Min-Yang.Lee@noaa.gov>
2025-05-02 12:56:14 -0500

    move workflow and recipe to separate .R file

---
+++ writing/BSB.Workflow.Setup.R
@@ -0,0 +1,107 @@
+
+# I'm using the tidymodels framework to train and test the classification trees and
+# Tiny bit of code to set up the BSB Workflow Recipe. 
+
+# I use this many times when I run different models, so it's good to have it in 1 place 
+
+
+
+
+#' # Model Definition and Baseline Fit
+#' 
+#' The model definition step declares the type of model (classification), engine (ranger), 
+#' and any options.  For this section, I'm setting 500 trees in the RF, no fewer than 5
+#' observations at the end of a branch, and 3 randomly selected variables.
+#' 
+#' I have left most of these options at their defaults. I'm noting things here 
+#' 
+#' Missing values are handled with the "na.learn" option (default). 
+#' 
+#' >With na.action = "na.learn", missing values are ignored for calculating an 
+#' initial split criterion value (i.e., decrease of impurity). Then for the best 
+#' split, all missings are tried in both child nodes and the choice is made based 
+#' again on the split criterion value.
+#' 
+#' @Hastie2009: The first is applicable to categorical predictors: we simply make a new category for “missing.”
+#' Second is construction of surrogate variables. When considering a predictor for a split, we use only the observations for which that predictor is not missing. Having chosen the best (primary) predictor and split point, we form a list of surrogate predictors and split points. The first surrogate is the predictor and corresponding split point that best mimics the split of the training data achieved by the primary split.  This is basically na.learn.
+#' 
+#' probability=FALSE. A probability forest (Malley et al 2012) might be a good idea
+#' 
+#' importance=NULL and splitrule=NULL.  This uses the Gini index as the impurity measure.
+#' 
+#' Geurts et al(2006)'s extremely random trees can be set with splitrule="extratrees".
+#' 
+#' trees=500. I don't have a good rationale for choose this.
+#' 
+#' Need to investigate the handling of the unordered factor covariates (@Hastie2009; Coppersmith et al 1999)
+#' 
+#' For an unordered predictor with $q$ levels, there are $2^{q-1}-1$ different ways to partition into two groups. This is not great.  For One way to handle this is to order the predictor levels in decreasing order of frequency and treat as if the factor was ordered.  This is proven to be the optimal way to handle unordered predictors for binary and quantitative (numeric) dependent variables.  It is not for multicategory outcomes @Hastie2009.
+#' 
+#' The partitioning algorithm tends to favor categorical predictors with many levels q; the number of partitions grows exponentially in q, and the more choices we have, the more likely we can find a good one for the data at hand. This can lead to severe overfitting if q is large, and such variables should be avoided @Hastie2009.
+#' 
+#'   * Coppersmith D., Hong S. J., Hosking J. R. (1999). Partitioning nominal attributes in decision
+#' trees. Data Min Knowl Discov 3:197-217. doi:10.1023/A:1009869804967.
+
+
+
+ranger_model<-rand_forest(mode="classification", trees = 500, min_n=5, mtry=3) %>%
+  set_engine("ranger",num.threads=!!my.ranger.threads, na.action="na.learn", respect.unordered.factors="order", importance="impurity")
+
+
+case_weights_allowed(ranger_model)
+
+
+# Use a workflow that combines the data processing recipe, assigns weights, and the model configuation
+BSB.Ranger.Workflow <-
+  workflow() %>%
+  add_model(ranger_model) %>% 
+  add_recipe(BSB.Classification.Recipe)
+
+
+# BSB.cf.Workflow <-
+#   workflow() %>%
+#   add_model(cf_model) %>% 
+#   add_recipe(BSB.Classification.Recipe)
+
+
+
+
+## Tuning
+# 
+# Set up a set of mtry to search over. This is not a great grid, but it's fine for now
+
+if (testing==TRUE){
+  mtry<-1:3
+  rf_grid<-as.data.frame(mtry)
+} else if (testing==FALSE) {
+  mtry<-1:20
+  mtry<-c(mtry,25,npredict)
+  rf_grid<-as.data.frame(mtry)
+}
+
+
+# configure the tuning part of the model.
+tune_spec <- rand_forest(
+  mtry = tune(),
+  trees = 500,
+  min_n = 5,
+) %>%
+  set_mode("classification") %>%
+  set_engine("ranger",num.threads=!!my.ranger.threads, na.action="na.learn", respect.unordered.factors="order", importance="impurity")
+
+
+
+
+
+# make a turning workflow. This combines the BSB.Classification.Recipe "data declaration" steps and new "tuning"
+# steps as the model.
+tune_wf <- workflow() %>%
+  add_recipe(BSB.Classification.Recipe) %>%
+  add_model(tune_spec)
+
+hardhat::extract_parameter_set_dials(tune_wf)
+
+# pass in a bunch of metrics
+# if the recipe/workflow is case_weight aware, the metrics are also case-weight aware
+class_and_probs_metrics <- metric_set(sensitivity, specificity, precision, bal_accuracy, mn_log_loss,average_precision, accuracy, brier_class, roc_auc)
+
--- writing/estimate_randomforest_nocluster.R
+++ writing/estimate_randomforest_nocluster.R
@@ -171,187 +171,15 @@ nrow(test_data)
 # 
 # The recipe simply defines the dataset, outcome (reponse, y) variable, id variables,
 # and predictor variables.
-
-
-# assign roles to predictors, outcome, groups, and weights
-BSB.Classification.Recipe <- recipe(train_data) %>%
-  update_role(market_desc, new_role = "outcome")%>%
-  update_role(c(dlrid,camsid), new_role = "ID variable") %>%
-  update_role(c(mygear,price,stockarea, state, year, month, semester, lndlb, grade_desc, trip_level_BSB), new_role = "predictor")
-
-# State-level daily Landings on "other" trips, by market category  
-BSB.Classification.Recipe <-BSB.Classification.Recipe %>%
-  update_role(c(StateOtherQJumbo, StateOtherQLarge, StateOtherQMedium, StateOtherQSmall), new_role = "predictor") 
-
-# stockarea-level daily Landings on "other" trips, by market category  
-BSB.Classification.Recipe <-BSB.Classification.Recipe %>%
-  update_role(c(StockareaOtherQJumbo, StockareaOtherQLarge, StockareaOtherQMedium, StockareaOtherQSmall), new_role = "predictor") 
-
-# Trailing 7 days landings, by stockarea and market category   
-BSB.Classification.Recipe <-BSB.Classification.Recipe %>%
-  update_role(c(MA7_StockareaQJumbo, MA7_StockareaQLarge, MA7_StockareaQMedium, MA7_StockareaQSmall), new_role = "predictor")
-
-# Trailing 7 days landings, by state and market category   
-BSB.Classification.Recipe <-BSB.Classification.Recipe %>%
-  update_role(c(MA7_StateQJumbo, MA7_StateQLarge, MA7_StateQMedium, MA7_StateQSmall), new_role = "predictor") 
-
-# Trailing 7 day trips, by state and stock area.   
-BSB.Classification.Recipe <-BSB.Classification.Recipe %>%
-  update_role(c(MA7_stockarea_trips, MA7_state_trips), new_role = "predictor") 
-
-# Dealer share of landings by market category from 2013-2017   
-BSB.Classification.Recipe <-BSB.Classification.Recipe %>%
-  update_role(c(Share2014Jumbo, Share2014Large, Share2014Medium,Share2014Small, Share2014Unclassified), new_role = "predictor") 
-
-# Dealer transaction count of landings by market category from 2013-2017   
-
-BSB.Classification.Recipe <-BSB.Classification.Recipe %>%
-  update_role(c(TransactionCountJumbo, TransactionCountLarge, TransactionCountMedium,TransactionCountSmall, TransactionCountUnclassified), new_role = "predictor") 
-
-# You can't center the factor variables
-# rescale and recenter 
-BSB.Classification.Recipe <- BSB.Classification.Recipe %>% 
-#  step_impute_knn(all_predictors()) %>%
-  step_center(all_numeric_predictors()) %>%
-  step_scale(all_numeric_predictors())
-
-recipe_summary<-BSB.Classification.Recipe %>%
-  summary() %>%
-  arrange(source,role, variable)
-
-recipe_summary
-
-#How many predictors
-npredict<-nrow(recipe_summary %>% dplyr::filter(role=="predictor"))
+source(here("writing","BSB.Classification.Recipe.R"))
 
 
 
-
-#' # Model Definition and Baseline Fit
-#' 
-#' The model definition step declares the type of model (classification), engine (ranger), 
-#' and any options.  For this section, I'm setting 500 trees in the RF, no fewer than 5
-#' observations at the end of a branch, and 3 randomly selected variables.
-#' 
-#' I have left most of these options at their defaults. I'm noting things here 
-#' 
-#' Missing values are handled with the "na.learn" option (default). 
-#' 
-#' >With na.action = "na.learn", missing values are ignored for calculating an 
-#' initial split criterion value (i.e., decrease of impurity). Then for the best 
-#' split, all missings are tried in both child nodes and the choice is made based 
-#' again on the split criterion value.
-#' 
-#' @Hastie2009: The first is applicable to categorical predictors: we simply make a new category for “missing.”
-#' Second is construction of surrogate variables. When considering a predictor for a split, we use only the observations for which that predictor is not missing. Having chosen the best (primary) predictor and split point, we form a list of surrogate predictors and split points. The first surrogate is the predictor and corresponding split point that best mimics the split of the training data achieved by the primary split.  This is basically na.learn.
-#' 
-#' probability=FALSE. A probability forest (Malley et al 2012) might be a good idea
-#' 
-#' importance=NULL and splitrule=NULL.  This uses the Gini index as the impurity measure.
-#' 
-#' Geurts et al(2006)'s extremely random trees can be set with splitrule="extratrees".
-#' 
-#' trees=500. I don't have a good rationale for choose this.
-#' 
-#' Need to investigate the handling of the unordered factor covariates (@Hastie2009; Coppersmith et al 1999)
-#' 
-#' For an unordered predictor with $q$ levels, there are $2^{q-1}-1$ different ways to partition into two groups. This is not great.  For One way to handle this is to order the predictor levels in decreasing order of frequency and treat as if the factor was ordered.  This is proven to be the optimal way to handle unordered predictors for binary and quantitative (numeric) dependent variables.  It is not for multicategory outcomes @Hastie2009.
-#' 
-#' The partitioning algorithm tends to favor categorical predictors with many levels q; the number of partitions grows exponentially in q, and the more choices we have, the more likely we can find a good one for the data at hand. This can lead to severe overfitting if q is large, and such variables should be avoided @Hastie2009.
-#' 
-#'   * Coppersmith D., Hong S. J., Hosking J. R. (1999). Partitioning nominal attributes in decision
-#' trees. Data Min Knowl Discov 3:197-217. doi:10.1023/A:1009869804967.
-
-
-
-ranger_model<-rand_forest(mode="classification", trees = 500, min_n=5, mtry=3) %>%
-  set_engine("ranger",num.threads=!!my.ranger.threads, na.action="na.learn", respect.unordered.factors="order", importance="impurity")
-
-
-case_weights_allowed(ranger_model)
-
-
-# Use a workflow that combines the data processing recipe, assigns weights, and the model configuation
-BSB.Ranger.Workflow <-
-  workflow() %>%
-  add_model(ranger_model) %>% 
-  add_recipe(BSB.Classification.Recipe)
-
-
-# BSB.cf.Workflow <-
-#   workflow() %>%
-#   add_model(cf_model) %>% 
-#   add_recipe(BSB.Classification.Recipe)
-
-
-# Fit the model on the entire training dataset. This of course, is not the best 
-# thing to do.  There's 2 parameters that we should search over mtry and min_n
-# to find the best model. But this is *a* model.
-
-# start_time_1fit<-Sys.time()
-# 
-# rf_fit <- 
-#   BSB.Ranger.Workflow %>% 
-#   fit(data = train_data)
-# 
-# end_time_1fit<-Sys.time()
-# 
-# end_time_1fit-start_time_1fit
-
-
-## Tuning
-# 
-# We will do 10 fold cross validation over the mtry parameter. We'll hold
-# trees constant. The number of trees isn't the most important thing, but we should eventually search over it.
+source(here("writing","BSB.Workflow.Setup.R"))
 
 set.seed(457)
 # split the training data group wise into 10 folds with the same number of observations, but grouped by dlrid, so that each dlrid is wholly contained in a single fold.
 myfolds<-rsample::vfold_cv(train_data, strata=market_desc, v = 10)
-
-# Set up a set of mtry to search over. This is not a great grid, but it's fine for now
-
-if (testing==TRUE){
-  mtry<-1:3
-  rf_grid<-as.data.frame(mtry)
-} else if (testing==FALSE) {
-  mtry<-1:20
-  mtry<-c(mtry,25,npredict)
-  rf_grid<-as.data.frame(mtry)
-}
-
-
-# configure the tuning part of the model.
-tune_spec <- rand_forest(
-  mtry = tune(),
-  trees = 500,
-  min_n = 5,
-) %>%
-  set_mode("classification") %>%
-  set_engine("ranger",num.threads=!!my.ranger.threads, na.action="na.learn", respect.unordered.factors="order", importance="impurity")
-
-
-
-
-
-# make a turning workflow. This combines the BSB.Classification.Recipe "data declaration" steps and new "tuning"
-# steps as the model.
-tune_wf <- workflow() %>%
-  add_recipe(BSB.Classification.Recipe) %>%
-  add_model(tune_spec)
-
-hardhat::extract_parameter_set_dials(tune_wf)
-
-# pass in a bunch of metrics
-# if the recipe/workflow is case_weight aware, the metrics are also case-weight aware
-class_and_probs_metrics <- metric_set(sensitivity, specificity, precision, bal_accuracy, mn_log_loss,average_precision, accuracy, brier_class, roc_auc)
-
-# Tune the model by passing in the workflow, folds and the rf_grid
-# I can look at the canned metrics in  https://yardstick.tidymodels.org/articles/metric-types.html
-# I'm more interested in the metrics based on "soft" predictions (class_prob). 
-# I will use the class probabilities on the back end, not a 'hard' prediction 
-# I'm interested in the 'aggregate predictions', not the 
-
-
 
 rf_control_grid<-control_grid(save_pred = TRUE, parallel_over="everything")
 start_time_tune<-Sys.time()
@@ -417,7 +245,7 @@ end_time_tune-start_time_tune
 # Select the best Rforest based on log loss from the 10 folds.  Do a final fit on the full training dataset, predict on the validation dataset. Save the data
 
 best_tree <- tune_res %>%
-  select_best(metric = "mn_log_loss")
+  select_best(metric = "brier_class")
 
 best_tree
 
