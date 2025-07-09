97fefac6011f405b574c2c88b06f86647c00e87b
mle2718 <Min-Yang.Lee@noaa.gov>
2025-07-09 14:28:14 -0400

    reorder

--- writing/batch_RF_run.sh
+++ writing/batch_RF_run.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 # batch file to run a bunch of RF models over the weekend
 
 Rscript --no-save --no-restore --verbose  ./estimate_randomforest.R > estimate_randomforest.log 2>&1
 
-Rscript --no-save --no-restore --verbose ./estimate_5class_nocluster_randomforest.R > estimate_5class_nocluster_randomforest.log 2>&1
-
 Rscript --no-save --no-restore --verbose  ./estimate_randomforest_nocluster.R > estimate_randomforest_nocluster.log 2>&1
 
-Rscript --no-save --no-restore --verbose ./estimate_5class_randomforest.R > estimate_5class_randomforest.log 2>&1
+# Rscript --no-save --no-restore --verbose ./estimate_5class_nocluster_randomforest.R > estimate_5class_nocluster_randomforest.log 2>&1
+
+# Rscript --no-save --no-restore --verbose ./estimate_5class_randomforest.R > estimate_5class_randomforest.log 2>&1
 
  
