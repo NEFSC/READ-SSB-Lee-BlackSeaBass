9d32056c9285853114ef3d16f8462016a8e7064c
mle2718 <Min-Yang.Lee@noaa.gov>
2025-07-21 14:33:52 -0500

    fix paths

--- R_code/analysis/fit_random_forest/estimate_5class_nocluster_randomforest.R
+++ R_code/analysis/fit_random_forest/estimate_5class_nocluster_randomforest.R
@@ -205,10 +205,10 @@ nrow(test_data)
 # 
 # The recipe simply defines the dataset, outcome (reponse, y) variable, id variables,
 # and predictor variables.
-source(here("analysis","fit_random_forest","BSB.Classification.Recipe.R"))
+source(here("R_code","analysis","fit_random_forest","BSB.Classification.Recipe.R"))
 
 
-source(here("analysis","fit_random_forest","BSB.Workflow.Setup.R"))
+source(here("R_code","analysis","fit_random_forest","BSB.Workflow.Setup.R"))
 set.seed(457)
 # split the training data group wise into 10 folds with the same number of observations, but grouped by dlrid, so that each dlrid is wholly contained in a single fold.
 myfolds<-rsample::vfold_cv(train_data, strata=market_desc, v = 10)
