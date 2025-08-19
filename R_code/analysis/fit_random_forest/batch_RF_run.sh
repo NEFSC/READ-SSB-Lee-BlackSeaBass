f6ad2de27391ffce0fa6be97f69e63a069bb82d2
mle2718 <Min-Yang.Lee@noaa.gov>
2025-08-19 12:47:59 -0500

    running various models from command line

--- R_code/analysis/fit_random_forest/batch_RF_run.sh
+++ R_code/analysis/fit_random_forest/batch_RF_run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # batch file to run a bunch of RF models over the weekend
+#Rscript --no-save --no-restore --verbose  ./estimate_randomforest_South_nocluster.R > estimate_randomforest_South_nocluster.log 2>&1
 
-Rscript --no-save --no-restore --verbose  ./estimate_randomforest.R > estimate_randomforest.log 2>&1
+#Rscript --no-save --no-restore --verbose ./estimate_randomforest_nocluster.R > estimate_randomforest_nocluster.log 2>&1
 
-Rscript --no-save --no-restore --verbose  ./estimate_randomforest_nocluster.R > estimate_randomforest_nocluster.log 2>&1
+#Rscript --no-save --no-restore --verbose  ./estimate_randomforest_North_nocluster.R > estimate_randomforest_North_nocluster.log 2>&1
 
 # Rscript --no-save --no-restore --verbose ./estimate_5class_nocluster_randomforest.R > estimate_5class_nocluster_randomforest.log 2>&1
 
 # Rscript --no-save --no-restore --verbose ./estimate_5class_randomforest.R > estimate_5class_randomforest.log 2>&1
 
  
+#Rscript --no-save --no-restore --verbose ./estimate_randomforest_RegionN_nocluster.R > estimate_randomforest_RegionN_nocluster.log 2>&1
+
+Rscript --no-save --no-restore --verbose  ./estimate_randomforest_RegionS_nocluster.R > estimate_randomforest_RegionS_nocluster.log 2>&1
