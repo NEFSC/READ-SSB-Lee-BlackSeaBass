a9f0cbe3b337ca4af1b00573a23877a24333d182
Emily Liljestrand <emily.liljestrand@noaa.gov>
2026-03-13 09:56:26 -0400

    Create CAA_calculation.R
    Testing pushing to my new branch

---
+++ R_code/CAA_calculation/CAA_calculation.R
@@ -0,0 +1,10 @@
+###############################################################################
+# Purpose: 	Code to apportion the unclassified market category into other MK and calculate catch at age
+
+
+# Inputs:
+#  - out of sample predictions from random forest
+# Outputs:
+#  - catch at age 
+
+###############################################################################  
