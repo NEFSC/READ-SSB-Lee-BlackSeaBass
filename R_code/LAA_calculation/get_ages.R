191205123aaac3b6d59e079b449bc26a3a480a10
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-04-27 10:21:47 -0400

    pass in landings.kg.name and caa.new.name for inputs and outputs. Add error handling.

--- R_code/LAA_calculation/get_ages.R
+++ R_code/LAA_calculation/get_ages.R
@@ -3,13 +3,14 @@ library(conflicted)
 conflicts_prefer(dplyr::filter())
 
 #' @title get_ages
-#' @description Take dataframe of catch by market category, stockarea,  and year
+#' @description Take dataframe of catch by SPECIES_ITIS, NESPP4, YEAR, SEX_TYPE, STOCK_ABBREV, REGION_ID, and BLOCK_ID
 #'    and return catch-at-age (CAA) by stock and year.
-#'
 #' @param species_itis A single character string specifying the ITIS code. Must match
 #'   SPECIES_ITIS values in out_of_sample_predictions.
 #' @param comm.land.res Data frame. Aggregate landings by block. Could be returned by get_stockeff() or constructed in an analogous way.  
 #' @param comm.land.length.age.res Data frame. Landings by length and age returned by get_stockeff().
+#' @param caa.new.name The name of the new column that will contains the Catch-at-Age returned by this function. 
+
 #'
 #' @return A data frame of catch-at-age with columns:
 #' \itemize{
@@ -35,7 +36,9 @@ conflicts_prefer(dplyr::filter())
 
 get_ages <- function(      species_itis             = NULL,
                            comm.land.res            = NULL,
-                           comm.land.length.age.res = NULL) {
+                           comm.land.length.age.res = NULL,
+                           landings.kg.name="LANDINGS_KG",
+                           caa.new.name = "CAA_NEW") {
 
   # --- Input validation -------------------------------------------------
   if (length(species_itis) != 1) stop("Only 1 species_itis code allowed")
@@ -43,7 +46,36 @@ get_ages <- function(      species_itis             = NULL,
 #  if (!all(out_of_sample_predictions$SPECIES_ITIS == species_itis)) {
 #    stop("species_itis doesn't match between requested and what provided by apportion file")
 #  }
-
+  
+  #Throw a helpful error message if the input dataframe is missing anything.
+  required_cols <- c("SPECIES_ITIS", "NESPP4", "YEAR", "SEX_TYPE", 
+                     "STOCK_ABBREV", "REGION_ID", "BLOCK_ID",landings.kg.name )
+  
+  missing_cols <- setdiff(required_cols, names(comm.land.res))
+  
+  if (length(missing_cols) > 0) {
+    stop(
+      "comm.land.res is missing required columns: ",
+      paste(missing_cols, collapse = ", ")
+    )
+  }
+  
+  if (!is.character(landings.kg.name) || length(landings.kg.name) != 1) {
+    stop("landings.kg.name must be a single character string, e.g., \"LANDINGS_KG\"")
+  }
+  
+  # --- End Input validation -------------------------------------------------
+  
+  
+  # --- BEGIN Output name validation -------------------------------------------------
+  if (!is.character(caa.new.name) || length(caa.new.name) != 1) {
+    stop("caa.new.name must be a single character string, e.g., \"CAA_NEW\"")
+  }
+  
+  
+  # --- End Input validation -------------------------------------------------
+  
+  
 
   # --- Join StockEff tables and reapportionment predictions -------------
   # Join order:
@@ -69,20 +101,17 @@ get_ages <- function(      species_itis             = NULL,
   #                        (WT_AT_AGE_LENGTH_NEW / IND_AVG_WT_KG)
   comm.land.length.age <- comm.land.length.age %>%
     mutate(
-      SCALING_FACTOR_NEW    = LANDINGS_KG / AVG_FISH_WT,
+      SCALING_FACTOR_NEW    = !!sym(landings.kg.name) / AVG_FISH_WT,
       WT_AT_LENGTH_NEW      = PROP_WT_LENGTH * SCALING_FACTOR_NEW,
       WT_AT_AGE_LENGTH_NEW  = WT_AT_LENGTH_NEW * PROP_AT_AGE,
       NO_AT_AGE_LENGTH_NEW  = WT_AT_AGE_LENGTH_NEW / IND_AVG_WT_KG
     )
 
   # --- Summarize to catch-at-age ----------------------------------------
-  # CAA_orig: original StockEff numbers at age (NO_AT_AGE_LENGTH), summed
-  #          across all length bins within stock/year/age.
-  # CAA_NEW: reapportioned numbers at age (NO_AT_AGE_LENGTH_NEW), same grouping that comes from 
+  # caa.new.name: apportioned numbers at age (NO_AT_AGE_LENGTH_NEW).
   land.CAA <- comm.land.length.age %>%
     group_by(SPECIES_ITIS,STOCK_ABBREV, YEAR, AGE) %>%
-    summarize(CAA_Orig = sum(NO_AT_AGE_LENGTH, na.rm=TRUE),
-              CAA_NEW = sum(NO_AT_AGE_LENGTH_NEW, na.rm=TRUE), .groups = "drop")
+    summarize(!!sym(caa.new.name) := sum(NO_AT_AGE_LENGTH_NEW, na.rm=TRUE), .groups = "drop")
 
 
   return(land.CAA)
