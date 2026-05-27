dd56be4fc84a99f8389a676e95eb01dbf9097cc0
mle2718 <Min-Yang.Lee@noaa.gov>
2026-05-27 16:25:02 -0500

    verify columns are good.

--- R_code/data_extraction_processing/processing/commercial/B01_data_prep_ml.R
+++ R_code/data_extraction_processing/processing/commercial/B01_data_prep_ml.R
@@ -519,6 +519,35 @@ estimation_dataset<-combined_dataset %>%
 write_rds(estimation_dataset, file=here("data_folder","main","commercial",glue("BSB_estimation_dataset{out_data_string}.Rds")))
 haven::write_dta(estimation_dataset, path=here("data_folder","main","commercial",glue("BSB_estimation_dataset{out_data_string}.dta")))
 
-# take a quick look
-zz<-estimation_dataset %>% filter(is.na(LagSharePoundsMedium))
-table(zz$year)
+
+
+# Make sure we have the same proper columns 
+# This mimics getting the part of the estimation code that gets ready to estimate. 
+# If I don't have the proper columns, the part that sources the Recipe will break.
+
+
+estimation_dataset<-estimation_dataset %>%
+  mutate(weighting = frequency_weights(weighting),
+         market_desc=fct_drop(market_desc))
+
+
+keep_cols<-c("market_desc","myl_id","dlrid","weighting", "mygear","price","priceR_CPI", "stockarea","state", "year","month", "semester","lndlb", "grade_desc", "trip_level_BSB", "catch_share")
+keep_cols<-c(keep_cols,"shore","nofederal")
+keep_cols<-c(keep_cols,"StateOtherQJumbo", "StateOtherQLarge", "StateOtherQMedium", "StateOtherQSmall" )
+keep_cols<-c(keep_cols,"StockareaOtherQJumbo", "StockareaOtherQLarge", "StockareaOtherQMedium", "StockareaOtherQSmall" )
+keep_cols<-c(keep_cols,"MA7_StockareaQJumbo", "MA7_StockareaQLarge", "MA7_StockareaQMedium", "MA7_StockareaQSmall" )
+keep_cols<-c(keep_cols,"MA7_StateQJumbo", "MA7_StateQLarge","MA7_StateQMedium", "MA7_StateQSmall")
+keep_cols<-c(keep_cols,"MA7_gearQJumbo", "MA7_gearQLarge","MA7_gearQMedium", "MA7_gearQSmall")
+keep_cols<-c(keep_cols,"MA7_stockarea_trips", "MA7_state_trips" )
+# keep_cols<-c(keep_cols,"Share2014Jumbo", "Share2014Large", "Share2014Medium","Share2014Small", "Share2014Unclassified" )
+# keep_cols<-c(keep_cols,"TransactionCountJumbo", "TransactionCountLarge", "TransactionCountMedium", "TransactionCountSmall", "TransactionCountUnclassified" )
+keep_cols<-c(keep_cols,"LagSharePoundsJumbo","LagSharePoundsLarge", "LagSharePoundsMedium","LagSharePoundsSmall")
+#keep_cols<-c(keep_cols,"LagShareTransJumbo", "LagShareTransLarge", "LagShareTransMedium","LagShareTransSmall", "LagShareTransUnclassified")
+keep_cols<-c(keep_cols, "Price_Diff_J","Price_Diff_L", "Price_Diff_M") 
+estimation_dataset<- estimation_dataset %>%
+  select(all_of(keep_cols))
+
+train_data<-estimation_dataset
+
+source(here("R_code","analysis","fit_random_forest","BSB.Classification.Recipe.R"))
+
