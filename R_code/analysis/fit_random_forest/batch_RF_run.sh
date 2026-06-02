934fa49ba7d0df6534988f61f828a3861ee0d09a
mle2718 <Min-Yang.Lee@noaa.gov>
2026-06-02 09:55:02 -0500

    adjust shell script for better loggin.

--- R_code/analysis/fit_random_forest/batch_RF_run.sh
+++ R_code/analysis/fit_random_forest/batch_RF_run.sh
@@ -3,7 +3,12 @@
 # batch file to run a bunch of RF models over the weekend
 #Rscript --no-save --no-restore --verbose ./estimate_randomforest_nocluster_Tsubset.R > estimate_randomforest_nocluster_Tsubset.log 2>&1
 
-Rscript --no-save --no-restore --verbose ./estimate_randomforest_nocluster.R > estimate_randomforest_nocluster.log 2>&1
+#Rscript --no-save --no-restore --verbose ./estimate_randomforest_nocluster.R > estimate_randomforest_nocluster.log 2>&1
+
+#Run from the project root
+Rscript --no-save --no-restore --verbose \
+  ./R_code/analysis/fit_random_forest/estimate_orandomforest_nocluster.R \
+  2>&1 | stdbuf -oL -eL tee ./results/h2o/estimate_orandomforest_nocluster.log
 
 
 # Rscript --no-save --no-restore --verbose ./estimate_5class_randomforest.R > estimate_5class_randomforest.log 2>&1
