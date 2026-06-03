2d2c1531b1c9beaf0d622834383d89ab93f90bff
mle2718 <Min-Yang.Lee@noaa.gov>
2026-06-03 13:57:18 -0500

    write a helper function for modeltype since you re-use that fragment so much.

---
+++ R_code/analysis/helpers/modeltype_patterns.R
@@ -0,0 +1,19 @@
+# set data patterns based on modeltype
+# You've estimated a few difference models, this sets the 
+
+if (modeltype=="standard"){
+  data_pattern<-"data_split"
+  tuning_pattern<-"BSB_ranger_tune"
+  final_pattern<-"BSB_ranger_results"
+} else if (modeltype=="nocluster"){
+  data_pattern<-"nocluster_data_split"
+  tuning_pattern<-"BSB_ranger_nocluster_tune"
+  final_pattern<-"BSB_ranger_nocluster_results"
+} else if (modeltype=="noclusterTEST"){
+  data_pattern<-"nocluster_data_split_TEST"
+  tuning_pattern<-"BSB_ranger_nocluster_tune_TEST"
+  final_pattern<-"BSB_ranger_nocluster_results_TEST"
+}else {
+  stop("Unknown modeltype")
+}
+
