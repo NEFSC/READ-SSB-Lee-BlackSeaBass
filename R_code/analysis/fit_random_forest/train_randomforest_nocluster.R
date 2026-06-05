e643111b283867ac0aa4d7252c4482e6e0e26c2a
mle2718 <Min-Yang.Lee@noaa.gov>
2026-06-05 13:32:36 -0500

    deal with comments.

--- R_code/analysis/fit_random_forest/train_randomforest_nocluster.R
+++ R_code/analysis/fit_random_forest/train_randomforest_nocluster.R
@@ -1,41 +1,20 @@
 ###############################################################################
-# Purpose: 	Estimate a Random Forest classification model on 4 classes WITHOUT Clustering
+# Purpose: 	Train a Random Forest classification model on 4 classes WITHOUT Clustering
 # on DLRID for the validation. Unclassified are excluded.
-
-# I'm using the tidymodels framework to train and test the classification trees and
-# random forest.  The main advantage is that switching models or estimation packages
-# (partykit::ctree vs ranger vs randomForest for example) is easier. Writing the model 
-# uses tidy syntax.  Tuning the model is made easier by using tune and yardstick.
-# Fitting ranger requires bonsai.
-# 
-# The canonical way to do this is to declare a recipe and a workflow.  Ideally,
-# everything would be part of the workflow, but my data processing skills in R are
-# note good enough to want to do this.  Therefore, I'm basically passing the
-# recipe into the workflow. C'est la guerre.
+# this assumes you have already trined a model using 
+# tune_randomforest_nocluster.R
 
 # Inputs:
-#  - BSB_estimation_dataset (from data_prep_ml.Rmd)
-#  - BSB_unclassified_dataset (from data_prep_ml.Rmd)
+#  - data_split (from tune_randomforest_nocluster.R)
 #  - BSB.Classification.Recipe.R
 #  - BSB.Workflow.Setup.R
 
 # Outputs:
-#  - estimating dataset 
-#  - tuning results 
 #  - final_fit results
+#  - variable importance
 ###############################################################################  
-# Set these two to control the size of the dataset. Useful for making sure code 
-# works.
 
 
-bayes_tune<-"FALSE"
-
-search_type<-"Initial"
-# search_type in "Initial", "Prototype","Advanced")
-
-# Only used with search_type<-"Prototype" -- how much data do you want in the dataset to prototype the code
-testing_fraction<-1			  
-#  
 start_time<-Sys.time()
 modeltype<-"nocluster"
 # OR "nocluster", or "fiveclass", or "noc5class" OR "standard"
@@ -78,7 +57,7 @@ conflicts_prefer(recipes::fixed())
 conflicts_prefer(recipes::step())
 conflicts_prefer(viridis::viridis_pal())
 conflicts_prefer(vip::vi)
-here::i_am("R_code/analysis/fit_random_forest/estimate_randomforest_nocluster.R")
+here::i_am("R_code/analysis/fit_random_forest/train_randomforest_nocluster.R")
 
 # Kill background logger if it is on
 system("pkill -f 'while true.*top'", ignore.stdout = TRUE, ignore.stderr = TRUE)
@@ -114,11 +93,10 @@ if (runClass %in% c('Local', 'Windows')){
   # Because the dataset is big, you are much better off doing 2 and 11 (or 1 and 22)
   my.parallel.threads<-2
   my.ranger.multi.threads<-11
-  my.ranger.sequential.threads<-20
+  my.ranger.sequential.threads<-22
   
   
 }
-my.ranger.threads<-5
 lbs_per_mt<-2204.62
 # my.parallel.threads =4 and my.ranger.multi.threads=5  about 30 GB of RAM on the "full" dataset (~381,542 in the training set).
 
@@ -149,57 +127,7 @@ if  (search_type=="Prototype"){
   
 }
 
-# Load data from data_prep_ml.Rmd
-estimation_dataset<-readr::read_rds(file=here("data_folder","main","commercial",glue("BSB_estimation_dataset{vintage_string}.Rds")))
-
-
-# for reproducibility
-set.seed(4587315)
-
-
-# construct the "case weights" variable here and trim out the extra factor levels from market_desc.
-estimation_dataset<-estimation_dataset %>%
-     mutate(weighting = frequency_weights(weighting),
-            market_desc=fct_drop(market_desc))
-
-# When testing, take a subset of the data. This is just to test how my code is working   
-if  (search_type=="Prototype"){
-  estimation_dataset$rand<-runif(nrow(estimation_dataset))
-  estimation_dataset<-estimation_dataset %>%
-    dplyr::filter(rand<=testing_fraction)
-}
-
-
-keep_cols<-c("market_desc","myl_id","dlrid","weighting", "mygear","price","priceR_CPI", "stockarea","state", "year","month", "semester","lndlb", "grade_desc", "trip_level_BSB", "catch_share")
-keep_cols<-c(keep_cols,"shore","nofederal")
-keep_cols<-c(keep_cols,"StateOtherQJumbo", "StateOtherQLarge", "StateOtherQMedium", "StateOtherQSmall" )
-keep_cols<-c(keep_cols,"StockareaOtherQJumbo", "StockareaOtherQLarge", "StockareaOtherQMedium", "StockareaOtherQSmall" )
-keep_cols<-c(keep_cols,"MA7_StockareaQJumbo", "MA7_StockareaQLarge", "MA7_StockareaQMedium", "MA7_StockareaQSmall" )
-keep_cols<-c(keep_cols,"MA7_StateQJumbo", "MA7_StateQLarge","MA7_StateQMedium", "MA7_StateQSmall")
-keep_cols<-c(keep_cols,"MA7_gearQJumbo", "MA7_gearQLarge","MA7_gearQMedium", "MA7_gearQSmall")
-keep_cols<-c(keep_cols,"MA7_stockarea_trips", "MA7_state_trips" )
-# keep_cols<-c(keep_cols,"Share2014Jumbo", "Share2014Large", "Share2014Medium","Share2014Small", "Share2014Unclassified" )
-# keep_cols<-c(keep_cols,"TransactionCountJumbo", "TransactionCountLarge", "TransactionCountMedium", "TransactionCountSmall", "TransactionCountUnclassified" )
-keep_cols<-c(keep_cols,"LagSharePoundsJumbo","LagSharePoundsLarge", "LagSharePoundsMedium","LagSharePoundsSmall", "first_dlr_year")
-#keep_cols<-c(keep_cols,"LagShareTransJumbo", "LagShareTransLarge", "LagShareTransMedium","LagShareTransSmall", "LagShareTransUnclassified")
-keep_cols<-c(keep_cols, "Price_Diff_J","Price_Diff_L", "Price_Diff_M") 
-
-
-estimation_dataset<- estimation_dataset %>%
-  select(all_of(keep_cols))
-
-set.seed(2824)
-# 70% of the data in the training, 15% in the calibration sample, 15% in the validation sample
-# consider splitting on strata=market_desc, although I don't think this is strictly necessary. 
-data_split <- initial_validation_split(
-  data=estimation_dataset,
-  prop = c(0.7, 0.15)
-)
-train_data <- training(data_split)
-test_data <- testing(data_split)
-validation_data <- validation(data_split)
-
-readr::write_rds(data_split, file=here("results","ranger",data_save_name))
+data_split<-readr::write_rds(data_split, file=here("results","ranger",data_save_name))
 
 nrow(train_data)
 nrow(test_data)
@@ -218,78 +146,11 @@ source(here("R_code","analysis","fit_random_forest","BSB.Classification.Recipe.R
 # Set up the tuning workflow
 source(here("R_code","analysis","fit_random_forest","BSB.Workflow.Setup.R"))
 
-set.seed(123)
-# split the training data group wise into 10 folds with the same number of observations, but grouped by dlrid, so that each dlrid is wholly contained in a single fold.
-myfolds<-rsample::vfold_cv(train_data, strata=market_desc, v = 10)
 
-plan("multisession", workers=my.parallel.threads)
-set.seed(8675309)					  
-
-rf_control_grid<-control_grid(save_pred = TRUE, parallel_over="everything")
-start_time_tune<-Sys.time()
-
-message("Tuning model hyperparameters", Sys.time())
-
-tune_res <- tune_grid(
-  BSB.Ranger.tuning.Workflow,
-  resamples = myfolds,
-  grid = rf_grid,
-  control=rf_control_grid,
-  metrics=class_and_probs_metrics
-)
-plan("sequential")
-message("Grid Tuning Finished", Sys.time())
+tune_res<-read_rds(file=here("results","ranger", tune_file_name))
 
 
-write_rds(tune_res, file=here("results","ranger", tune_file_name))
-end_time_tune<-Sys.time()
-end_time_tune-start_time_tune
-
-bayes_param <- BSB.Ranger.tuning.Workflow %>% 
-  extract_parameter_set_dials() %>% 
-  update(mtry = finalize(mtry(), train_data))
-
-if(bayes_tune==TRUE){
-message("Performing Bayesian Tuning", Sys.time())
-  
-  # Do a tune_bayes
-  plan("multisession", workers=my.parallel.threads)
-  set.seed(9035768)
-  
-  start_time_bt<-Sys.time()
-  
-  tune_res2 <- tune_bayes(
-    object=BSB.Ranger.tuning.Workflow,
-    resamples = myfolds,
-    initial = tune_res,
-    param_info=bayes_param,
-    iter = 30,                     # 
-    control = control_bayes(
-      verbose = TRUE,
-      no_improve=10,
-      save_pred = TRUE,             # Save predictions for analysis
-      save_workflow = FALSE,        # Save memory
-      extract = NULL,              # Don't extract additional info
-      parallel_over = "everything" # Parallelize over resamples to save memory
-      ),
-      metrics=metric_set(brier_class)
-  )
-  end_time_bt<-Sys.time()
-  end_time_bt-start_time_bt
-  
-  plan("sequential")
-  write_rds(tune_res2, file=here("results","ranger", tune_file_name))
-  message("Bayesian Tuning Finished", Sys.time())
-  
-  autoplot(tune_res2, type = "performance") +
-    labs(title = "Did Bayesian optimization converge?")
-}else if (bayes_tune==FALSE){
-  message("Bayesian Tuning Skipped")
-  
-  tune_res2<-tune_res
-}
-
-tune_res2 %>%
+tune_res %>%
   tune::collect_notes() %>%
   dplyr::filter(type == "error") %>%
   dplyr::select(location, note) %>%
@@ -298,7 +159,7 @@ tune_res2 %>%
 
 # Select the best Rforest based on log loss from the 10 folds.  Do a final fit on the full training dataset, predict on the validation dataset. Save the data
 
-best_params <- tune_res2 %>%
+best_params <- tune_res %>%
   select_best(metric = "brier_class")
 
 best_params
