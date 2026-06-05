4590bc17b7123b252b973831b965fe53ffb04991
mle2718 <Min-Yang.Lee@noaa.gov>
2026-06-05 14:10:25 -0500

    tidy up.

--- R_code/analysis/fit_random_forest/batch_RF_run.sh
+++ R_code/analysis/fit_random_forest/batch_RF_run.sh
@@ -6,10 +6,10 @@ set -o pipefail
 #Rscript --no-save --no-restore --verbose ./estimate_randomforest_nocluster.R > estimate_randomforest_nocluster.log 2>&1
 
 #Run from the project root
-Rscript --no-save --no-restore --verbose \
-  ./R_code/analysis/fit_random_forest/tune_randomforest_nocluster.R \
-  2>&1 | stdbuf -oL -eL tee ./results/ranger/tune_randomforest_nocluster.log\
-&& \
+ Rscript --no-save --no-restore --verbose \
+   ./R_code/analysis/fit_random_forest/tune_randomforest_nocluster.R \
+   2>&1 | stdbuf -oL -eL tee ./results/ranger/tune_randomforest_nocluster.log\
+ && \
 Rscript --no-save --no-restore --verbose \
   ./R_code/analysis/fit_random_forest/train_randomforest_nocluster.R \
   2>&1 | stdbuf -oL -eL tee ./results/ranger/train_randomforest_nocluster.log\
