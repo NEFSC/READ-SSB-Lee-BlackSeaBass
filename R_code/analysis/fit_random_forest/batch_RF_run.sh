7daa47b2f6086c9bbbc27065cec5ad18af2fd8c7
mle2718 <Min-Yang.Lee@noaa.gov>
2026-06-18 12:41:35 -0500

    go back to old version

--- R_code/analysis/fit_random_forest/batch_RF_run.sh
+++ R_code/analysis/fit_random_forest/batch_RF_run.sh
@@ -7,13 +7,13 @@ set -o pipefail
 #   2>&1 | stdbuf -oL -eL tee ./results/ranger/tune_randomforest_nocluster.log #\
 #&& \
 # Train \
- Rscript --no-save --no-restore --verbose \
-   ./R_code/analysis/fit_random_forest/train_randomforest_nocluster.R >  \
-   ./results/ranger/train_randomforest_nocluster.log 2>&1
-
+Rscript --no-save --no-restore --verbose \
+  ./R_code/analysis/fit_random_forest/train_randomforest_nocluster.R \
+  2>&1 | stdbuf -oL -eL tee ./results/ranger/train_randomforest_nocluster.log \
+&& \
 # Fit the Variable importance 
  Rscript --no-save --no-restore --verbose \
-   ./R_code/analysis/fit_random_forest/variable_importance_randomforest_nocluster.R >  \
-   ./results/ranger/variable_importance_randomforest_nocluster.log 2>&1
+   ./R_code/analysis/fit_random_forest/variable_importance_randomforest_nocluster.R \
+  2>&1 | stdbuf -oL -eL tee ./results/ranger/variable_importance_randomforest_nocluster.log
 
 
