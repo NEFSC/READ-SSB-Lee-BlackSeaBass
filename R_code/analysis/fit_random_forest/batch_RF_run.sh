eb192ffdc2b28323bd5e627206c80613b2961981
mle2718 <Min-Yang.Lee@noaa.gov>
2026-07-01 07:12:12 -0500

    add comments for the entire execution pipeline

--- R_code/analysis/fit_random_forest/batch_RF_run.sh
+++ R_code/analysis/fit_random_forest/batch_RF_run.sh
@@ -12,8 +12,24 @@ set -o pipefail
 #  2>&1 | stdbuf -oL -eL tee ./results/ranger/train_randomforest_nocluster.log #\
 #&& \
 # Fit the Variable importance 
- Rscript --no-save --no-restore --verbose \
-   ./R_code/analysis/fit_random_forest/variable_importance_randomforest_nocluster.R \
-  2>&1 | stdbuf -oL -eL tee ./results/ranger/variable_importance_randomforest_nocluster.log
+# Rscript --no-save --no-restore --verbose \
+#   ./R_code/analysis/fit_random_forest/variable_importance_randomforest_nocluster.R \
+#  2>&1 | stdbuf -oL -eL tee ./results/ranger/variable_importance_randomforest_nocluster.log
 
+#################################################
+# Perform the weighted calibration
+#################################################
+ # Rscript --no-save --no-restore --verbose \
+ #   ./R_code/analysis/fit_random_forest/weighted_calibration.R \
+ #  2>&1 | stdbuf -oL -eL tee ./R_code/analysis/fit_random_forest/weighted_calibration.log
+
+######################
+# Make figures
+# /writing/figure1.R
+# /writing/figure2.R  (needs to be run in windows only)
+# /writing/predictions_heatmap.R
+
+# tuning_diagnostics.Rmd makes ROC curves for the best tuned model and final model
+# out_of_sample_predictions.Rmd makes out of sample predictions.
+########################
 
