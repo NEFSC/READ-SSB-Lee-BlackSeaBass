6d9bd194a1168d3a8342b47c74efec7369a40ff8
mle2718 <Min-Yang.Lee@noaa.gov>
2026-07-08 09:27:44 -0500

    add widget of tuning results.

--- R_code/analysis/fit_tilefish_random_forest/fit_tilefish_classification.R
+++ R_code/analysis/fit_tilefish_random_forest/fit_tilefish_classification.R
@@ -243,6 +243,26 @@ tune_res <- tune_grid(
 )
 message("Grid Tuning Finished at", Sys.time())
 
+# Save the tuning results to an interactive html widget that we can use to explore
+# tuning.  Low loss is good, so we want to be at the low point.  
+tune_metrics<-tune_res  %>%
+  collect_metrics() %>%
+  filter(.metric == "mn_log_loss") %>%
+  select(mean, mtry, min_n) %>%
+  rename(mn_log_loss=mean)
+
+
+p<- plot_ly(tune_metrics, 
+            x = ~mtry, 
+            y = ~min_n, 
+            z = ~mn_log_loss,
+            type = "mesh3d", 
+            intensity=~mn_log_loss,
+            colorscale="Hot",
+            reversescale=TRUE
+)
+saveWidget(p, here("results","ranger","tune",glue("tilefish_mn_log_loss_{tuning_pattern}{tuning_vintage}.html")), selfcontained = TRUE)
+rm(p)
 
 
 
