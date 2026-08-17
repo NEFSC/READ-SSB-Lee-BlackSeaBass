3ab393f788b1167f032bbf9d1978873ea86d6087
Emily Liljestrand <emily.liljestrand@noaa.gov>
2026-08-17 10:27:43 -0400

    added laa_calculation_bsb

---
+++ R_code/LAA_calculation/LAA_calculation_BSB.R
@@ -0,0 +1,215 @@
+#' @title LAA_calculation
+#' @description Use reapportionment information to recalculate Landings-at-age
+#' @param species_itis A string specifying a single itis code to query stockeff 
+#' @param out_of_sample_predictions A data frame specifying reapportioned metric tons across market categories. This dataframe must be be zero padded.  If there are 3 market categories in every year in the original data, there must be 3 in this dataframe. 
+#' \itemize{
+#'   \item{YEAR}
+#'   \item{SEMESTER}
+#'   \item{STOCK_ABBREV} 
+#'   \item{MARKET_DESC_ORIG}
+#'   \item{SPECIES_ITIS}
+#'   \item{MARKET_DESC}
+#'   \item{LANDINGS_KG_CATEGORY_APPORTION}
+#' }
+#' 
+#' 
+#' @param fyr first year for which you want LAA data
+#' @param lyr last year for which you want LAA data
+#' @param connection the connection information to access stockeff using sql 
+#'
+#' @return A data frame of the landings at age 
+#' \itemize{
+#'   \item{indices - FILL IN }
+#'   \item{strata_means_summary - FILL IN}
+#' }
+#' @examples
+#' CAA<- LAA_calculation(species_itis = '167687',
+#'                             out_of_sample_predictions = readRDS(here("data_folder","predictions", glue("out_of_sample_predictions_YRS_nocluster{predictions_vintage}.Rds"))),
+#'                             fyr = 1989,
+#'                             lyr = 2024,
+#'                             connection = connection)
+
+# Library and Source Code/Functions
+library(ROracle)
+library(tidyverse)
+library(here)
+library(glue)
+library("conflicted")
+conflicts_prefer(dplyr::filter())
+
+species_itis = '167687'
+out_of_sample_predictions = readRDS(here("data_folder","predictions", "out_of_sample_predictions_YRS_nocluster2026-06-15.rds"))
+fyr=1989
+lyr=2024
+connection=db1
+
+
+LAA_calculation <- function(species_itis = NULL,
+                            out_of_sample_predictions = NULL,
+                            fyr = NULL,
+                            lyr = NULL,
+                            connection = NULL){
+
+  if(length(species_itis)!=1){stop("Only 1 species_itis code allowed")}
+  else if(unique(out_of_sample_predictions$SPECIES_ITIS) != species_itis){stop("species_itis doesn't match between requested and what provided by apportion file")}
+  else{
+    
+  ################################################################################
+  # Query StockEff
+  ################################################################################
+  
+  # Query the market categories from StockEff:
+  mkt.qry <- glue("select NESPP4, market_desc 
+                          from stockeff.e_cf_market_c 
+                          where species_itis in ({species_itis})" )
+  mkt.res <- fetch(dbSendQuery(connection, mkt.qry))
+  # Query the aggregate landings from StockEff:
+  
+  comm.land.qry <- glue("select * 
+                          from stockeff.mv_cf_stock_caa_land_block_o 
+                          where species_itis in ({species_itis}) and year between {fyr} and {lyr}
+                          and LANDINGS_KG IS NOT NULL")
+  
+  
+  
+  comm.land.res <- fetch(dbSendQuery(connection, comm.land.qry))
+  
+  # If there's nothing there, then its likely the stock is in PREproduction, not production in stockeff:
+  if(dim(comm.land.res)[1]==0){
+    comm.land.qry <- glue("select * 
+                          from stockeff_pre_prod.mv_cf_stock_caa_land_block_o 
+                          where species_itis in ({species_itis}) and year between {fyr} and {lyr}
+                          and LANDINGS_KG IS NOT NULL")
+    
+    
+    
+    comm.land.res <- fetch(dbSendQuery(connection, comm.land.qry))
+  }
+  # Query the landings by age and length from StockEff:
+  comm.land.length.age.qry <- glue("select * from stockeff.v_cf_stock_caa_num_len_age_o 
+                                   where SPECIES_ITIS in ({species_itis})  and year between {fyr} and {lyr}")
+  comm.land.length.age.res <- fetch(dbSendQuery(connection, comm.land.length.age.qry))
+  
+  if(dim(comm.land.length.age.res)[1]==0){
+    comm.land.length.age.qry <- glue("select * from stockeff_pre_prod.v_cf_stock_caa_num_len_age_o 
+                                   where SPECIES_ITIS in ({species_itis})  and year between {fyr} and {lyr}")
+    comm.land.length.age.res <- fetch(dbSendQuery(connection, comm.land.length.age.qry))
+  }
+  #create a marker flag on in out_of_sample_predictions to make the logic of subsequent case_when a little safer.
+  out_of_sample_predictions <- out_of_sample_predictions %>% 
+    mutate(has_rf_pred = 1)
+  ## Combine the stockeff files and apportionment file
+  comm.land.length.age <- comm.land.res  %>% 
+    left_join(comm.land.length.age.res, by=join_by(SPECIES_ITIS, NESPP4, YEAR, SEX_TYPE, STOCK_ABBREV, REGION_ID, BLOCK_ID)) %>% 
+    left_join(mkt.res, by=join_by(NESPP4)) %>% 
+    left_join(out_of_sample_predictions, by=join_by(SPECIES_ITIS, YEAR,STOCK_ABBREV, MARKET_DESC, BLOCK_ID==SEMESTER))
+  
+  # CONSTRUCT LANDINGS_KG_ADJUSTED
+  # is.na(has_rf_pred) Where the isn't an RF prediction, use the original landings from stockeff (LANDINGS_KG)
+  # MARKET_DESC=="UNCLASSIFIED" Where the original market category is unclassified, use the new apportion value.  This is correct because 
+  ##    1.  The RF model may "decline" to make a classifcation for some unclassified observations and we want to retain them as UNCLASSIFIED here. 
+  ##    2.  THe RF model may classify "all" the unclassifeds. When it does, the value of LANDINGS_KG_CATEGORY_APPORTION is zero.
+  # MARKET_DESC!="UNCLASSIFIED", add category apportion value to the original landings
+  # MARKET_DESC_ORIG refers the original market category and MARKET_DESC is the RF classified new market category.
+  
+  comm.land.length.age <- comm.land.length.age %>% 
+    mutate(LANDINGS_KG_ADJUSTED = case_when(
+      is.na(has_rf_pred) ~ LANDINGS_KG,
+      MARKET_DESC=="UNCLASSIFIED" ~ LANDINGS_KG_CATEGORY_APPORTION,
+      MARKET_DESC!="UNCLASSIFIED" ~ LANDINGS_KG+LANDINGS_KG_CATEGORY_APPORTION)
+    ) 
+  
+  ################################################################################
+  # Calculate weight at age and length using LANDINGS_KG_ADJUSTED
+  ################################################################################
+  
+
+  comm.land.length.age <- comm.land.length.age %>% mutate(SCALING_FACTOR_NEW = LANDINGS_KG_ADJUSTED/AVG_FISH_WT)
+  comm.land.length.age <- comm.land.length.age %>% mutate(WT_AT_LENGTH_NEW = PROP_WT_LENGTH*SCALING_FACTOR_NEW)
+  comm.land.length.age <- comm.land.length.age %>% mutate(WT_AT_AGE_LENGTH_NEW = WT_AT_LENGTH_NEW*PROP_AT_AGE)
+  comm.land.length.age <- comm.land.length.age %>% mutate(NO_AT_AGE_LENGTH_NEW = WT_AT_AGE_LENGTH_NEW/IND_AVG_WT_KG)
+  
+  ################################################################################
+  # RETURN
+  ################################################################################
+
+  #################
+  # Landings at Age
+  #################
+  land.CAA.OLD <- comm.land.length.age %>%
+    group_by(SPECIES_ITIS, STOCK_ABBREV, YEAR, AGE) %>%
+    summarize(CAA = sum(NO_AT_AGE_LENGTH)) %>%
+    mutate(CAA_TYPE = 'Original') %>%
+    ungroup()
+  
+  land.CAA.NEW <- comm.land.length.age %>%
+    group_by(SPECIES_ITIS,STOCK_ABBREV, YEAR, AGE) %>%
+    summarize(CAA = sum(NO_AT_AGE_LENGTH_NEW)) %>%
+    mutate(CAA_TYPE = 'Apportioned') %>%
+    ungroup()
+  
+  land.CAA <- land.CAA.OLD %>% full_join(land.CAA.NEW)
+  
+  fyr.plot <- 2020
+  land.CAA.yrs <- land.CAA %>% filter(YEAR>=fyr.plot)
+  
+  CAA_PLOT <- ggplot(data=land.CAA.yrs,aes(x=AGE,y=CAA,col=CAA_TYPE,fill = CAA_TYPE)) + 
+    geom_col(position = "identity", alpha = 0.5) + 
+    theme_classic() +
+    facet_grid(YEAR ~ STOCK_ABBREV)
+  CAA_PLOT
+  
+  ggsave(
+    filename = "CAA_PLOT.png",
+    plot = CAA_PLOT,
+    width = 15,         # Define your width
+    height = 10,        # Define your height
+    units = "in",       # Options: "in", "cm", "mm", "px"
+    dpi = 300,          # High resolution for print
+    limitsize = FALSE   # Crucial: bypasses the 50x50 inch cap
+  )
+  
+  
+  #################
+  # Landings at Length
+  #################
+  
+  land.CAL.OLD <- comm.land.length.age %>%
+    group_by(SPECIES_ITIS, STOCK_ABBREV, YEAR, LENGTH) %>%
+    summarize(CAL = sum(NO_AT_AGE_LENGTH,na.rm=T)) %>%
+    mutate(CAL_TYPE = 'Original') %>%
+    ungroup()
+  land.CAL.OLD <- land.CAL.OLD %>% group_by(YEAR) %>% summarise(CATCH = sum(CAL)) %>% full_join(land.CAL.OLD) %>% mutate(CAL_PROP = CAL/CATCH)
+  
+  land.CAL.NEW <- comm.land.length.age %>%
+    group_by(SPECIES_ITIS,STOCK_ABBREV, YEAR, LENGTH) %>%
+    summarize(CAL = sum(NO_AT_AGE_LENGTH_NEW,na.rm=T)) %>%
+    mutate(CAL_TYPE = 'Apportioned') %>%
+    ungroup()
+  land.CAL.NEW <- land.CAL.NEW %>% group_by(YEAR) %>% summarise(CATCH = sum(CAL)) %>% full_join(land.CAL.NEW) %>% mutate(CAL_PROP = CAL/CATCH)
+  
+  
+  land.CAL <- land.CAL.OLD %>% full_join(land.CAL.NEW)
+  
+  fyr.plot <- 2020
+  land.CAL.yrs <- land.CAL %>% filter(YEAR>=fyr.plot)
+  
+  CAL_PLOT <- ggplot(data=land.CAL.yrs,aes(x=LENGTH,y=CAL_PROP,col=CAL_TYPE,fill = CAL_TYPE)) + 
+    geom_col(position = "identity", alpha = 0.5) + 
+    theme_classic() +
+    facet_grid(YEAR ~ STOCK_ABBREV)
+  CAL_PLOT
+  
+  ggsave(
+    filename = "CAL_PLOT.png",
+    plot = CAL_PLOT,
+    width = 15,         # Define your width
+    height = 10,        # Define your height
+    units = "in",       # Options: "in", "cm", "mm", "px"
+    dpi = 300,          # High resolution for print
+    limitsize = FALSE   # Crucial: bypasses the 50x50 inch cap
+  )
+  
+  return(land.CAA)
+  }
+}
