84aea4662ecbcf8c8862fe0186ded1e74252d942
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-08-19 14:06:44 -0400

    fix db connection

--- R_code/LAA_calculation/test_refactor.R
+++ R_code/LAA_calculation/test_refactor.R
@@ -13,10 +13,10 @@ species_itis = '167687'
 out_of_sample_predictions = readRDS(here("data_folder","predictions", "out_of_sample_predictions_YRS_nocluster2026-06-15.rds"))
 fyr=1989
 lyr=2024
-#connection=db1
+connection=db1
 
-drv<-dbDriver("Oracle")
-connection<-eval(nefscdb_con)
+#drv<-dbDriver("Oracle")
+#connection<-eval(nefscdb_con)
 
 
 # Testing procedure for refactor:
