3916bc0b8257c20bcaa3ed9665881db99c59b857
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2026-08-27 17:00:39 -0400

    turn everything on in this wrapper.

--- R_code/analysis/fit_random_forest/batch_RF_run.sh
+++ R_code/analysis/fit_random_forest/batch_RF_run.sh
@@ -2,19 +2,19 @@
 set -o pipefail
 # batch file to the Tune, train, Variable importance code
 # Tune. Run from the project root
-# Rscript --no-save --no-restore --verbose \
-#   ./R_code/analysis/fit_random_forest/tune_randomforest_nocluster.R \
-#   2>&1 | stdbuf -oL -eL tee ./results/ranger/tune_randomforest_nocluster.log #\
+ Rscript --no-save --no-restore --verbose \
+   ./R_code/analysis/fit_random_forest/tune_randomforest_nocluster.R \
+   2>&1 | stdbuf -oL -eL tee ./results/ranger/tune_randomforest_nocluster.log 
 #&& \
 # Train \
-#Rscript --no-save --no-restore --verbose \
-#  ./R_code/analysis/fit_random_forest/train_randomforest_nocluster.R \
-#  2>&1 | stdbuf -oL -eL tee ./results/ranger/train_randomforest_nocluster.log #\
+Rscript --no-save --no-restore --verbose \
+  ./R_code/analysis/fit_random_forest/train_randomforest_nocluster.R \
+  2>&1 | stdbuf -oL -eL tee ./results/ranger/train_randomforest_nocluster.log 
 #&& \
 # Fit the Variable importance 
-# Rscript --no-save --no-restore --verbose \
-#   ./R_code/analysis/fit_random_forest/variable_importance_randomforest_nocluster.R \
-#  2>&1 | stdbuf -oL -eL tee ./results/ranger/variable_importance_randomforest_nocluster.log
+ Rscript --no-save --no-restore --verbose \
+   ./R_code/analysis/fit_random_forest/variable_importance_randomforest_nocluster.R \
+  2>&1 | stdbuf -oL -eL tee ./results/ranger/variable_importance_randomforest_nocluster.log
 
 #################################################
 # Perform the weighted calibration
