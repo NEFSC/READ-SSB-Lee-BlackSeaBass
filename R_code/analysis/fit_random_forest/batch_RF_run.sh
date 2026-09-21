96b25f056b04fa3ac1b5976424c36af7f78101a2
mle2718 <Min-Yang.Lee@noaa.gov>
2026-09-21 13:31:42 -0500

    Add figure of prevailing prices

--- R_code/analysis/fit_random_forest/batch_RF_run.sh
+++ R_code/analysis/fit_random_forest/batch_RF_run.sh
@@ -48,6 +48,14 @@ Rscript --no-save --no-restore --verbose \
  #   ./writing/figure2.R \
  #  2>&1 | stdbuf -oL -eL tee ./writing/figure2.log
 
+
+######################
+# Make figures
+ Rscript --no-save --no-restore --verbose \
+   ./writing/figure_prevailing_price.R \
+  2>&1 | stdbuf -oL -eL tee ./writing/figure_prevailing_price.log &&
+
+
 # Predictions heatmap
 Rscript -e "rmarkdown::render('writing/predictions_heatmap.Rmd', output_format='html_document')" &&
 
